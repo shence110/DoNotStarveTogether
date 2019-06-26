23e8d9fdc33205d2ebc45a92d2880e92ab89255f
ce shen <shence110@vip.qq.com>
2019-06-26 17:20:36 +0800

    1

---
+++ DoNotStarveTogetherRail/Cluster_1/Caves/modoverrides.lua
@@ -0,0 +1,58 @@
+return {
+  ["workshop-100010002"]={
+    configuration_options={
+      amount_of_numbers="default",
+      display_mode="waving",
+      dmg_only="on",
+      show_decimal_points="no" 
+    },
+    enabled=true 
+  },
+  ["workshop-100010018"]={ configuration_options={ kong=0, maxdiaoluo=4, mindiaoluo=1 }, enabled=true },
+  ["workshop-100010022"]={
+    configuration_options={ dont_give_dubloons=2, give_dubloons=1, position=1, rec_dubloon=3, rec_gold=3 },
+    enabled=true 
+  },
+  ["workshop-100010023"]={
+    configuration_options={
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  },
+  ["workshop-100010024"]={
+    configuration_options={
+      Craft="Easy",
+      Destroyable="yees",
+      FoodSpoilage=0,
+      Language="En",
+      Position="Center",
+      Slots=80 
+    },
+    enabled=true 
+  },
+  ["workshop-100010027"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
+  ["workshop-100010034"]={ configuration_options={  }, enabled=true },
+  ["workshop-100010058"]={
+    configuration_options={ DFV_Language="CN", DFV_MinimalMode="default" },
+    enabled=true 
+  },
+  ["workshop-100010059"]={ configuration_options={  }, enabled=true },
+  ["workshop-100010062"]={
+    configuration_options={
+      divider=5,
+      random_health_value=0,
+      random_range=0,
+      send_unknwon_prefabs=true,
+      show_type=0,
+      unknwon_prefabs=1,
+      use_blacklist=true 
+    },
+    enabled=true 
+  },
+  ["workshop-2199027653598519491"]={ configuration_options={  }, enabled=true } 
+}
---
+++ DoNotStarveTogetherRail/Cluster_3/Caves/modoverrides.lua
@@ -0,0 +1,51 @@
+return {
+  ["workshop-100010002"]={
+    configuration_options={
+      amount_of_numbers="default",
+      display_mode="waving",
+      dmg_only="on",
+      show_decimal_points="no" 
+    },
+    enabled=true 
+  },
+  ["workshop-100010023"]={
+    configuration_options={
+      FIREOPTIONS=2,
+      OVERRIDEMODE=false,
+      SHAREMINIMAPPROGRESS=true,
+      SHOWFIREICONS=true,
+      SHOWPLAYERICONS=true,
+      SHOWPLAYERSOPTIONS=2 
+    },
+    enabled=true 
+  },
+  ["workshop-100010024"]={
+    configuration_options={
+      Craft="Easy",
+      Destroyable="yees",
+      FoodSpoilage=0,
+      Language="En",
+      Position="Center",
+      Slots=80 
+    },
+    enabled=true 
+  },
+  ["workshop-100010027"]={ configuration_options={ ["Draw over FoW"]="disabled" }, enabled=true },
+  ["workshop-100010058"]={
+    configuration_options={ DFV_Language="CN", DFV_MinimalMode="default" },
+    enabled=true 
+  },
+  ["workshop-100010062"]={
+    configuration_options={
+      divider=5,
+      random_health_value=0,
+      random_range=0,
+      send_unknwon_prefabs=true,
+      show_type=0,
+      unknwon_prefabs=1,
+      use_blacklist=true 
+    },
+    enabled=true 
+  },
+  ["workshop-2199027653598512025"]={ configuration_options={ uses=10000000000 }, enabled=true } 
+}
